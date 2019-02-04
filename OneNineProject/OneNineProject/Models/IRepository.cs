8517cf7522a39adee389612f046963313df5d9e0
Chopko T.Y <tarik.freeman@gmail.com>
2019-02-04 18:50:00 +0200

    Delated realization of IRepository and OneNineRepository

--- OneNineProject/OneNineProject/Models/IRepository.cs
+++ OneNineProject/OneNineProject/Models/IRepository.cs
@@ -8,24 +8,6 @@ namespace OneNineProject.Models
 {
     public interface IRepository
     {
-        void Add(Doc doc);
-
-        void Add(Person person);
-
-        void Add(Executor executor);
-
-        List<Doc> DocsToList();
-
-        List<Person> PersonsToList();
-
-        List<Executor> ExecutorsToList();
-
-        Doc FindFullDoc(int? id);
-
-        Person FindPerson(int id);
-
-        Executor FindExecutor(int id);
-
-        void Save();
+       
     }
 }
--- OneNineProject/OneNineProject/Models/OneNineProjectContext.cs
+++ OneNineProject/OneNineProject/Models/OneNineProjectContext.cs
@@ -24,83 +24,10 @@ namespace OneNineProject.Models
     {
         private OneNineProjectContext db = new OneNineProjectContext();
 
-        public void Add(Doc doc)
-        {
-            db.Docs.Add(doc);
-        }
+       
+      
 
-        public void Add(Person person)
-        {
-            db.Persons.Add(person);
-        }
+       
 
-        public Doc FindFullDoc(int? id)
-        {
-            try
-            {
-                Doc findedDoc = db.Docs
-                    .Include(p => p.Persons)
-                    .Include(e => e.Executors)
-                    .SingleOrDefault(r => r.DocId == id);
-                return findedDoc;
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-        }
-
-        public Person FindPerson(int id)
-        {
-            Person findedPerson = db.Persons.Find(id);
-
-            if (findedPerson == null)
-            {
-                return null;
-            }
-            else
-            {
-                return findedPerson;
-            }
-        }
-
-        public Executor FindExecutor(int id)
-        {
-            Executor findedEx = db.Executors.Find(id);
-            if (findedEx == null)
-            {
-                return null;
-            }
-            else
-            {
-                return findedEx;
-            }
-        }
-
-        public void Add(Executor executor)
-        {
-            db.Executors.Add(executor);
-        }
-
-        public List<Doc> DocsToList()
-        {
-            var IncludedDoc = db.Docs.Include(p => p.Persons).Include(e => e.Executors);
-            return IncludedDoc.ToList();
-        }
-
-        public List<Person> PersonsToList()
-        {
-            return db.Persons.ToList();
-        }
-
-        public List<Executor> ExecutorsToList()
-        {
-            return db.Executors.ToList();
-        }
-
-        public void Save()
-        {
-            db.SaveChanges();
-        }
     }
 }
